4681cef8903296e8cf912b4b2a4eefb589acd4b7
SumGuy14 <sumedhkulkarni14@gmail.com>
2026-07-01 16:32:17 -0400

    error enum added

---
+++ movieDB/Constants/NetworkState.swift
@@ -0,0 +1,12 @@
+enum NetworkError: String, Error  {
+    case invalidURL = "Invalid URL"
+    case noData = "No Data Received From Server"
+    case decodingError = "Decoding Error"
+    case serverError = "Server Error"
+}
+
+enum NetworkState<T> {
+    case loading
+    case successful(data: T)
+    case failure(error: NetworkError)
+}
---
+++ movieDB/Extensions/UiViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+extension UIViewController{
+    func showError(message: String){
+        let alert = UIAlertController(
+            title: "Error",
+            message: message,
+            preferredStyle: .alert
+        )
+
+        let action = UIAlertAction(
+            title: "OK",
+            style: .default
+        )
+        
+        alert.addAction(action)
+        DispatchQueue.main.async { [weak self] in
+            self?.present(alert, animated: true)
+        }
+        
+    }
+}
